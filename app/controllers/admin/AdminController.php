66bcec74de93e19dfa9a2df97effe2220eeae998
Robert Mordido <rdmordido@gmail.com>
2015-02-02 14:15:18 +0800

    Issue #3 - fixed bug found in logout function

--- app/controllers/admin/AdminController.php
+++ app/controllers/admin/AdminController.php
@@ -41,9 +41,9 @@ class AdminController extends BaseController {
 	 * @return Response
 	 */
 	public function logout(){
-		if(Auth::user()->history()->create(['event'=>'logged out'])){
+		if(Auth::user() && Auth::user()->history()->create(['event'=>'logged out'])){
 			Auth::logout();
-        	return Redirect::to('admin/login');
 		}
+		return Redirect::to('admin/login');
     }
 }
